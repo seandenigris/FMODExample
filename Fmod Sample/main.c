c727093c19840fa450200120cc5142278cc27034
Sean DeNigris <sean@clipperadams.com>
2013-12-02 15:46:07 -0500

    Initial commit

---
+++ Fmod Sample/main.c
@@ -0,0 +1,63 @@
+//
+//  main.c
+//  Fmod Sample
+//
+//  Created by Sean DeNigris on 11/21/13.
+//  Copyright (c) 2013 Sean DeNigris. All rights reserved.
+//
+
+#define FMOD_H "/Volumes/Sean/Smalltalk/Sandbox/FMOD/FMOD Programmers API/api/lowlevel/inc/fmod.h"
+#define SOUND_FILE_PATH "/Users/sean/Documents/Microsoft User Data/Entourage Script Menu Items/trainwhistle.mp3"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include FMOD_H
+
+int exitOnError(int errCode)
+{
+    exit(errCode);
+}
+
+int main(int argc, const char * argv[])
+{
+
+    FMOD_SYSTEM* system;
+    FMOD_RESULT err = FMOD_System_Create(&system);
+    if (err != 0) exitOnError(err);
+    
+    err = FMOD_System_Init(system, 32, FMOD_INIT_NORMAL, 0);
+    if (err != 0) exitOnError(err);
+    
+    FMOD_SOUND * sound;
+    err = FMOD_System_CreateSound(system, SOUND_FILE_PATH,
+                                        FMOD_HARDWARE,
+                                        0,
+                                        &sound);
+    if (err != 0) exitOnError(err);
+    
+    FMOD_CHANNEL *channel;
+    FMOD_System_PlaySound(
+                          system,
+                          sound,
+                          0,
+                          0,
+                          &channel
+                          );
+    if (err != 0) exitOnError(err);
+    
+    FMOD_BOOL isPlaying = 1;
+    while (isPlaying) {
+        FMOD_Channel_IsPlaying(channel, &isPlaying);
+    }
+
+    err = FMOD_Sound_Release(sound);
+    if (err != 0) exitOnError(err);
+    err = FMOD_System_Close(system);
+    if (err != 0) exitOnError(err);
+    err = FMOD_System_Release(system);
+    if (err != 0) exitOnError(err);
+    
+    printf("Returned %d\n", err);
+    return 0;
+}
+
